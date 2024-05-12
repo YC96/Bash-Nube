94c88c43be981c30bcd6377cf95e38451fcd2d8a
Yoikerth Cádiz <67969403+YC96@users.noreply.github.com>
2024-05-12 16:01:47 -0400

    Backend sh
    arcvhivo bash para configuracion del backend en los contenedores

---
+++ backend.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+for i in $(seq 1 3); do
+    lxc exec "backend${i}" -- /bin/bash -c "apt-get update && apt-get install -y npm"
+
+    lxc exec "backend${i}" -- /bin/bash -c "git clone https://github.com/manuelenrq9/CRUDubuntu.git && ls"
+
+    lxc exec "backend${i}" -- /bin/bash -c "cd CRUDubuntu && npm install"
+
+    # production mode
+    lxc exec "backend${i}" -- /bin/bash -c "cd CRUDubuntu && npm run start:prod"
+done
+
