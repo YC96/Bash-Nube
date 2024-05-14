bf64aaf7afaff6b8fad58d38c7ba635dbeaf49f5
manuelenrq9 <manuel.enrqt@gmail.com>
2024-05-14 09:07:32 -0400

    script for container creation added

--- backend.sh
+++ backend.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 for i in $(seq 1 3); do
+
+    lxc launch ubuntu:22.04 "backend${i}"
+
     lxc exec "backend${i}" -- /bin/bash -c "apt-get update && apt-get install -y npm"
 
     lxc exec "backend${i}" -- /bin/bash -c "git clone https://github.com/manuelenrq9/CRUDubuntu.git && ls"
