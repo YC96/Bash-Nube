b60e63bc4e725b24f6b90c350631939f4676fe17
manuelenrq9 <manuel.enrqt@gmail.com>
2024-05-14 21:06:08 -0400

    configuration modified

--- backend.sh
+++ backend.sh
@@ -25,29 +25,21 @@ user www-data;
 worker_processes auto;
 pid /run/nginx.pid;
 include /etc/nginx/modules-enabled/*.conf;
-
 events {
-        worker_connections 768;
-        # multi_accept on;
+worker_connections 768;
+# multi_accept on;
 }
-
 http {
-    upstream myapp1 {
-        server backend1.lxd;
-        server backend2.lxd;
-        server backend3.lxd;
-    }
-
-    server {
-        listen 80;
-
-        location / {
-            proxy_pass http://myapp1;
-        }   
-    }
+upstream myapp1 {
+server backend1.lxd;
+server backend2.lxd;
+server backend3.lxd;
+}
+server {
+listen 80;
+location / {
+proxy_pass http://myapp1;
+}
+}
 }
 EOF"
-
-lxc exec loadbalancer -- /bin/bash -c "sudo nginx -t"
-
-lxc exec loadbalancer -- /bin/bash -c "sudo systemctl restart nginx"
