6f5c39a39d683052dcc23b4c3df2458591fc4e75
manuelenrq9 <manuel.enrqt@gmail.com>
2024-05-16 20:23:36 -0400

    nest cli installation added

--- backend.sh
+++ backend.sh
@@ -4,6 +4,8 @@ for i in $(seq 1 3); do
 
     lxc launch ubuntu:22.04 "backend${i}"
 
+    lxc exec "backend${i}" -- /bin/bash -c "npm install -g @nestjs/cli"
+
     lxc exec "backend${i}" -- /bin/bash -c "apt-get update && apt-get install -y npm"
 
     lxc exec "backend${i}" -- /bin/bash -c "git clone https://github.com/manuelenrq9/CRUDubuntu.git && ls"
